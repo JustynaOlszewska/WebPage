786162761b48c9858f930e934feb380034b18790
Justyna Olszewka <wioza@wp.pl>
2020-02-14 18:53:22 +0100

    Updates

---
+++ precache-manifest.7d59a733416b4c6bbde543db69ad2233.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c5b611ee3b7f98ee42c93eccaeab6b",
+    "url": "/WebPage/index.html"
+  },
+  {
+    "revision": "2d5682ae263cc464c835",
+    "url": "/WebPage/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "08da412cd6d0af21443f",
+    "url": "/WebPage/static/css/main.6fab08ad.chunk.css"
+  },
+  {
+    "revision": "2d5682ae263cc464c835",
+    "url": "/WebPage/static/js/2.659c2db9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/WebPage/static/js/2.659c2db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08da412cd6d0af21443f",
+    "url": "/WebPage/static/js/main.33a25cce.chunk.js"
+  },
+  {
+    "revision": "b23f7c35fea000352e5a",
+    "url": "/WebPage/static/js/runtime-main.d890fc2a.js"
+  },
+  {
+    "revision": "88d938cf037c1e5be6fed32586cd334f",
+    "url": "/WebPage/static/media/business-woman-2697954_640.88d938cf.jpg"
+  },
+  {
+    "revision": "cb9ca14bb2a5dc24ee20f36a16d615a2",
+    "url": "/WebPage/static/media/girl-1208207_640.cb9ca14b.jpg"
+  },
+  {
+    "revision": "fcc47100ffe3981f2ce2d82442609aa8",
+    "url": "/WebPage/static/media/keyboard-622456_1920.fcc47100.jpg"
+  },
+  {
+    "revision": "7a3bfd425f3627fff09bef6371f7f28c",
+    "url": "/WebPage/static/media/laptop-698609_1920.7a3bfd42.jpg"
+  },
+  {
+    "revision": "7cc685d7795722cf8c99a4f1f84c36f2",
+    "url": "/WebPage/static/media/man-537136_640.7cc685d7.jpg"
+  },
+  {
+    "revision": "424653fd8c41e1eed097ff2f4b629504",
+    "url": "/WebPage/static/media/woman-3055841_640.424653fd.jpg"
+  }
+]);
